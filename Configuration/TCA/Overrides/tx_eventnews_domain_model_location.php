232b91b49848b33737ab72d4fb115e097b5cb364
Jaume Presas <jpresas@w-4.com>
2023-11-18 00:18:35 +0100

    [FIX] Some fixes for events and logo

---
+++ Configuration/TCA/Overrides/tx_eventnews_domain_model_location.php
@@ -0,0 +1,6 @@
+<?php
+
+defined('TYPO3') or die();
+
+$GLOBALS['TCA']['tx_eventnews_domain_model_location']['columns']['lat']['config']['default'] = 0;
+$GLOBALS['TCA']['tx_eventnews_domain_model_location']['columns']['lng']['config']['default'] = 0;
